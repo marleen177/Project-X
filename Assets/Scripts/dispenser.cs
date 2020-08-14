7c177d9ab56a7675a8aa094c4b87d6907f4a4f2d
marleen177 <marleenvandersteege@gmail.com>
2020-08-14 13:35:31 +0200

    counter start
    only changed the text on the shelf

--- Assets/Scripts/dispenser.cs
+++ Assets/Scripts/dispenser.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class dispenser : MonoBehaviour
 {
+
+    public TextMeshProUGUI txt;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "herb")
+        {
+            Debug.Log("collision works"); //nice
+
+            txt.text = "1";
+
+
+            //Destroy(col.gameObject);
+        }
     }
 }
